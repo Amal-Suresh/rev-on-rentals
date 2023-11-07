0ef5f528a7194fb52f15d5364a7cecd2807da042
Amal-Suresh <amalsureshms2001@gmail.com>
2023-11-07 23:41:16 +0530

    add coupon completed

--- controller/couponController.js
+++ controller/couponController.js
@@ -3,16 +3,29 @@ const Coupon = require('../models/couponModel')
 
 const addCoupon =async (req,res)=>{
     try {
-
-        
-        
+        const {couponName,couponCode,discountValue,minPurchase,maxDiscount,expireDate,limit} = req.body
+        const coupon = new Coupon({
+            couponName,
+            couponCode,
+            discountValue,
+            minPurchase,
+            maxDiscount,
+            expireDate,
+            limit 
+        }) 
+        const addCoup = await coupon.save(); 
+        if(addCoup){
+            res.status(200).send({success:true, message:"coupon added successfully"})
+        }else{
+            res.status(201).send({success:false, message:"failed to add coupon"})
+        }
     } catch (error) {
         console.log(error.message);
-        
+        res.status(500).send({success:false, message:"something went wrong"})  
     }
 }
 
 
 module.exports={
-
+    addCoupon
 }
