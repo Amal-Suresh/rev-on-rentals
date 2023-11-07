2e8585f6ac9e723559f52f83b3a5a5e65d813db0
Amal-Suresh <amalsureshms2001@gmail.com>
2023-11-07 23:07:46 +0530

    coupon model added

---
+++ models/couponModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const couponSchema = new mongoose.Schema({
+    couponName:{
+        type:String,
+        required:true
+    },
+    couponCode:{
+        type:String,
+        required:true
+    },
+    discountValue:{
+        type:Number,
+        required:true
+    },
+    whoUsed:{
+        type:Array,
+    },
+    minPurchase:{
+        type:Number,
+        required:true,
+    },
+    maxDiscount:{
+        type:Number,
+        required:true,
+    },
+    expireDate:{
+        type:Date,
+        default:false
+    },
+    status:{
+        type:Boolean,
+        default:true
+    },
+    limit:{
+        type:Number,
+        required:true
+    }
+})
+module.exports =mongoose.model('coupon',couponSchema);
