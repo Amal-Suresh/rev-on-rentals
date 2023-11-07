0ade1a67801d2d6a1fc0b2d1c3dbcd39f0a897c1
Amal-Suresh <amalsureshms2001@gmail.com>
2023-11-07 23:12:20 +0530

    coupon  contoller created successfully

---
+++ controller/couponController.js
@@ -0,0 +1,18 @@
+const User = require('../models/userModel')
+const Coupon = require('../models/couponModel')
+
+const addCoupon =async (req,res)=>{
+    try {
+
+        
+        
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+}
+
+
+module.exports={
+
+}
